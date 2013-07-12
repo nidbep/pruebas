103ea26d40bcdee5a1d4062f0551fc0860f7cf90
Nidia Benitez <nidbep@hotmail.com>
2013-07-12 13:08:21 -0500

    vxdgsd
    dfgsd

--- js/API/conexion.js
+++
@@ -1,9 +0,0 @@
-// Conexión
-function isConnected(){
-	//se utiliza una funcion de phonegap para saber si hay conexion a internet en 
-	//el dispositivo
-	if(navigator.connection.type!=Connection.NONE)
-		return true;
-	else
-		return false;
-}
